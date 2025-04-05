f4118a5c7db96e9fbed35835b29dea4d62c5466c
Mario Niko <marios-2222@hotmail.com>
2025-04-05 13:23:37 +0300

    implement categories backend logic update/delete/create

--- the-shop-admin/src/app/admin/categories/page.tsx
+++ the-shop-admin/src/app/admin/categories/page.tsx
@@ -1,10 +1,10 @@
 import { getCategoriesWithProducts } from "@/actions/categories";
+import CategoryPageComponent from "@/app/admin/categories/page-component";
+
+// This is a use server by default so we fetch the data on the server which is faster and secure and then we pass them on the client component
 
 export default async function Categories() {
   const categories = await getCategoriesWithProducts();
-  
-  console.log(categories);
-  
-  
-  return <div>Categories</div>;
+
+  return <CategoryPageComponent categories={categories} />;
 }
